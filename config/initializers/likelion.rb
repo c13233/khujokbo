e2839bd09052130b3285a504934a91af20e10942
c13233 <c13233@naver.com>
2018-03-31 03:55:20 +0000

    directory 이름 그대로

--- config/initializers/likelion.rb
+++ config/initializers/likelion.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     region:                'ap-northeast-2',                                    # optional, defaults to 'us-east-1'
     endpoint:              'https://s3.ap-northeast-2.amazonaws.com'            # optional, defaults to nil
   }
-  config.fog_directory  = ENV['AWS_BUCKET']                                   # required
+  config.fog_directory  = 'khujokbo'                                   # required
   config.fog_public     = true                                                    # optional, defaults to true
   config.fog_attributes = {} # optional, defaults to {}
   
