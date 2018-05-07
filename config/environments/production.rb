bd5d01ab391272f1db3903f55283476fa9874e1f
EC2 Default User <ec2-user@ip-172-31-27-129.ap-northeast-2.compute.internal>
2018-05-07 04:28:58 +0000

    pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
